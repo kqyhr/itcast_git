029cb6c35c51f050f036ea6d809477a351133062
yanghao <1363817756@qq.com>
2019-12-29 01:21:12 +0800

    修改git

--- src/main/java/com/itheima/git.java
+++ src/main/java/com/itheima/git.java
@@ -4,4 +4,9 @@ public class git {
     public static void main(String[] args) {
         System.out.println("hello,git");
     }
+    public void test01(){
+        int i=0;
+        i+=10;
+        System.out.println(i);
+    }
 }
